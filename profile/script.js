3c304791af039f570117f045affa9dce00edbae9
ARNAB BOSE <sirarnab.bose.2006@gmail.com>
2024-02-13 16:15:28 +0530

    Working for contact, done

--- profile/script.js
+++ profile/script.js
@@ -10,16 +10,21 @@ document.getElementById("send").addEventListener("click", function (e) {
 });
 
 function sending() {
-    // Email.send({
-    //     Host : "smtp.gmail.com",
-    //     Username : "sirarnab.bose.2006@gmail.com",
-    //     Password : "yztkhgrcuglveezy",
-    //     To : "latikabose1976@gmail.com",
-    //     From : mail.value,
-    //     Subject : "New from submitted",
-    //     Body : "Hello Guys"
-    // }).then(
-    //     message => alert(message)
-    // );
-    console.log("sending function");
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "sirarnab.bose.2006@gmail.com",
+        Password : "97AD5DB8410FFC9F733586A02E8EF0BC68C0",
+        To : "sirarnab.bose.2006@gmail.com",
+        From : "sirarnab.bose.2006@gmail.com",
+        Subject : "New from submitted",
+        Body : "<div style='width: 100%; height: auto; background-color: #2c8fec; box-sizing: border-box; padding: 10px 0px; border-radius: 5px;'><div style='text-align: center; color: #fff; font-size: 30px; font-weight: bold; padding: 30px 0px;'>MASTERMIND</div><div style='box-sizing: border-box; background: #fff; padding: 10px; margin: 10px; border-radius: 5px;'><span style='color: #0f62af; font-weight: bold;'>Name : </span>" + nam.value + "</div><div style='box-sizing: border-box; background: #fff; padding: 10px; margin: 10px; border-radius: 5px;'><span style='color: #0f62af; font-weight: bold;'>Email : </span>" + mail.value + "</div><div style='box-sizing: border-box; background: #fff; padding: 10px; margin: 10px; border-radius: 5px; text-align: justify;'>" + mas.value + "</div></div>"
+    }).then(
+        message => {
+            if(message == "OK"){
+                alert("Your message is successfully submitted");
+            }else{
+                alert("Some error occurred, please try again");
+            }
+        }
+    );
 }
