1acc972ee8b4c445dcc293118f1ccdeb273df6e0
arnabbose313 <112298944+arnabbose313@users.noreply.github.com>
2024-03-14 15:34:18 +0530

    Code for server production done. (#1)
    * All files upadted for server
    
    * css2.css added for calc.html
    
    * css2.css file updated
    
    * Display updated
    
    * js1.js updated
    
    * Hello id corrected
    
    * Working for no thanks
    
    * Update 1
    
    * ncd.js is removed
    
    * Updated 2
    
    * Updated 3
    
    * Updated 4
    
    * Update 5
    
    * Working for contact, done
    
    * Working for contact updated
    
    * Redeem page making 1
    
    * Redeem code design css complited
    
    * Profile folder deleted
    
    * Privacy, contact, terms and condition created for production

---
+++ js3.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    document.getElementById("hello").setAttribute("style", "display: block;");
+    document.getElementById("coin").innerHTML = window.atob(nts);
+    setInterval(function () {
+        if (window.navigator.onLine == false) {
+            window.location.href = "logo.html?net";
+        }
+    }, 1000);
+    $("#photo1").click(function () {
+        window.location.href = "profile.html";
+    });
+    $(".sty").click(function () {
+        window.location.href = "calc.html?type=" + this.id;
+    });
+});
